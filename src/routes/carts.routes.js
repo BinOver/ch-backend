73f94a287928ca21359f7194914bec193b596768
Arturo Perez Suarez <binover@gmail.com>
2023-09-13 20:33:58 -0300

    2da pre-entrega - carts

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -35,9 +35,74 @@ cartRouter.post('/:cid/products/:pid',async (req,res) => {
   const { quantity } = req.body;
   try {
     const cart =  await cartModel.findById(cid);
+    
     if (cart) {
       cart.products.push({ id_prod:pid, quantity:quantity});
+      await cart.save();
       const respuesta = await cartModel.findByIdAndUpdate(cid,cart);
+      res.status(200).send({ resultado: 'OK', carrito: respuesta });
+    }
+  }
+  catch (err) {
+      res.status(400).send({error: `Error al crear carrito ${err}`});
+  }
+})
+
+cartRouter.put('/:cid/products/:pid',async (req,res) => {
+  const { cid, pid } = req.params;
+  const { quantity } = req.body;
+
+  try {
+    const cart =  await cartModel.findById(cid);
+    if (cart){
+      const indexToUpdate = cart.products.findIndex(producto => producto.id_prod._id.toString() === pid);
+      cart.products[indexToUpdate].quantity = quantity;
+      cart.save();
+      res.status(200).send({ resultado: 'OK', carrito: cart.products[indexToUpdate] });
+    }else{
+      res.status(404).send({ resultado: 'Carrito no encontrado', carrito: cart });
+    }
+  }
+  catch (err) {
+      res.status(400).send({error: `Error al consultar carrito ${err}`});
+  }
+})
+
+cartRouter.put('/:cid',async (req,res) => {
+  const { cid } = req.params;
+  const { products } = req.body;
+
+  try {
+    const cart =  await cartModel.findById(cid);
+    if (cart){
+      products.forEach(nuevoProducto => {
+        const indexOfProduct = cart.products.findIndex(producto => producto.id_prod._id.toString() === nuevoProducto.id_prod);
+        if(indexOfProduct !== -1){
+          cart.products[indexOfProduct].quantity = nuevoProducto.quantity;
+        }else{
+          cart.products.push(nuevoProducto);
+        }
+      });
+      res.status(200).send({ resultado: 'OK', carrito: cart.products });
+    }else{
+      res.status(404).send({ resultado: 'Carrito no encontrado', carrito: cart });
+    }
+    await cart.save();
+  }
+  catch (err) {
+      res.status(400).send({error: `Error al consultar carrito ${err}`});
+  }
+})
+
+cartRouter.delete('/:cid/products/:pid',async (req,res) => {
+  const { cid, pid }  = req.params;
+  try {
+    const cart =  await cartModel.findById(cid);
+    if (cart) {
+      const indexToRemove = cart.products.findIndex(producto => producto.id_prod._id.toString() === pid);
+      cart.products.splice(indexToRemove,1);
+      await cart.save();
+      const respuesta = await cartModel.find();
       res.status(200).send({ resultado: 'OK', carrito: respuesta });
     }
   }
@@ -49,17 +114,33 @@ cartRouter.post('/:cid/products/:pid',async (req,res) => {
 cartRouter.delete('/:cid',async (req,res) => {
   const { cid }  = req.params;
   try {
-    const respuesta = await cartModel.findByIdAndDelete(cid);
-    if(respuesta){
+    const cart =  await cartModel.findById(cid);
+    if (cart) {
+      cart.products= [];
+      await cart.save();
+      const respuesta = await cartModel.find();
       res.status(200).send({ resultado: 'OK', carrito: respuesta });
-    }else{
-      res.status(404).send({ resultado: 'Carrito no encontrado', carrito: cart });
     }
   }
   catch (err) {
       res.status(400).send({error: `Error al crear carrito ${err}`});
   }
 })
+
+// cartRouter.delete('/:cid',async (req,res) => {
+//   const { cid }  = req.params;
+//   try {
+//     const respuesta = await cartModel.findByIdAndDelete(cid);
+//     if(respuesta){
+//       res.status(200).send({ resultado: 'OK', carrito: respuesta });
+//     }else{
+//       res.status(404).send({ resultado: 'Carrito no encontrado', carrito: cart });
+//     }
+//   }
+//   catch (err) {
+//       res.status(400).send({error: `Error al crear carrito ${err}`});
+//   }
+// })
 
 
 export default cartRouter;
